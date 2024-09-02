684db35448516b432560c6943044987f0c252b8d
Saikat-too <saikatsingha00q@gmail.com>
2024-09-02 10:28:26 +0600

    Initial version of videa showing from the desire file ,, but nor workin !!!

--- videogen.py
+++ videogen.py
@@ -1,3 +1,37 @@
 #!/usr/bin/python3
 
-print("Hello World")
+import cv2
+
+
+def vid_play():
+    vid_capture = cv2.VideoCapture("test.mkv")
+
+    if not vid_capture.isOpened():
+        print("Error Opening The File")
+
+    else:
+        fps = vid_capture.get(5)
+        print("Frame Per  Second : ", fps, "FPS")
+
+        frame_count = vid_capture.get(7)
+        print("Frame Count : ", frame_count)
+
+        while vid_capture.isOpened():
+            ret, frame = vid_capture.read()
+            if ret:
+                cv2.imshow("Frame", frame)
+
+                key = cv2.waitKey(20)
+
+                if key == ord("q"):
+                    break
+
+                else:
+                    break
+
+    vid_capture.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    vid_play()
