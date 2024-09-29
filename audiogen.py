cc4bb1327c13b79bc06d61166d6b0072801ecbbd
Saikat-too <saikatsingha00q@gmail.com>
2024-09-29 15:28:21 +0600

    update gitignore

--- audiogen.py
+++ audiogen.py
@@ -3,34 +3,70 @@
 from moviepy.editor import VideoFileClip
 import tkinter as tk
 from tkinter import filedialog
+from pygame import mixer
 
 
 class Music:
     def __init__(self, window, video_title):
         self.window = window
-        self.video_title = video_title 
+        self.video_title = video_title
         self.window.title(video_title)
-        self.audio = None  # Initialize audio attribute
-        
+        self.window.geometry("600x400")
+        self.video = None
+        self.playing = False
+        self.paused = False
+        self.playback_time = 0
         # Create buttons
-        self.load_button = tk.Button(self.window, text="Load video", command=self.load_video)
+        self.load_button = tk.Button(
+            self.window, text="Load video", command=self.load_video
+        )
         self.load_button.pack(pady=20)
-        self.play_button = tk.Button(self.window, text="Play Audio", command=self.play_audio)
+        self.play_button = tk.Button(
+            self.window, text="Play Audio", command=self.play_audio
+        )
         self.play_button.pack(pady=20)
-    
+        self.pause_button = tk.Button(
+            self.window, text="Pause Audio", command=self.pause_audio
+        )
+        self.pause_button.pack(pady=20)
+        self.stop_button = tk.Button(
+            self.window, text="Stop Audio", command=self.stop_audio
+        )
+        self.stop_button.pack(pady=20)
+        self.exit_button = tk.Button(self.window, text="Exit", command=self.exit)
+        self.exit_button.pack(pady=20)
+
     def load_video(self):
         # Ask the user to select a video file
         video_path = filedialog.askopenfilename(filetypes=[("MP4 files", "*.mp4")])
         if video_path:
-            video = VideoFileClip(video_path)
-            self.audio = video.audio
+            mixer.music.load(video_path)
             print("Audio loaded.")
-    
+
     def play_audio(self):
-        if self.audio:
-            self.audio.preview()
+        if self.paused:
+            mixer.music.unpause()
+
         else:
-            print("No audio loaded.")
+            mixer.music.play(-1)
+
+    def pause_audio(self):
+        if self.playing:
+            self.playing = False
+            self.paused = True
+
+        mixer.music.pause()
+
+    def stop_audio(self):
+        self.playing = False
+        self.paused = False
+        mixer.music.stop()
+        print("Audio Stopped")
+
+    def exit(self):
+        self.window.quit()
+        self.window.destroy()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
